88d8ff13a6003b82768a9ae6d5f89a53d3e96409
gineslopez11 <glopezpena@udesa.edu.ar>
2026-03-20 17:05:31 -0300

    entrenamientos pseudo inv y grad codeados

--- src/models.py
+++ src/models.py
@@ -1,5 +1,22 @@
 import numpy as np
 import pandas as pd
 
-#class LinearRegression():
+class LinearRegression:
+	def __init__(self, X, y):
+		self.X = np.column_stack((np.ones(X.shape[0]), X))
+		self.y = y
 	
+	def entrenar_pseudo_inv(self):
+		self.w = np.linalg.inv((self.X).T @ self.X) @ (self.X).T @ self.y 
+		return self.w
+	
+	def entrenar_gradiente_descendiente(self,alpha,iters):
+		self.w = np.zeros((self.X).shape[1])
+		n = (self.X).shape[0]
+
+		for _ in range (iters):
+			y_pred = self.X @ self.w
+			grad = (2/n)*(self.X).T @ (y_pred - self.y)
+			self.w = self.w - alpha*grad
+		
+		return self.w
